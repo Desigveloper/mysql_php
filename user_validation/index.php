a37f1abe9cf479c4a5218021ebbeaa93d829a125
Habibu Mamudu <habibumamudu@gmail.com>
2024-03-22 11:06:27 +0000

    Added style to page

--- user_validation/index.php
+++ user_validation/index.php
@@ -6,37 +6,41 @@ echo <<<_END
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="style.css">
             <title>Login Form - User Validation</title>
         </head>
         <body><center>
-            <form action="user.php" method="POST" name="contactForm">
-                <table border="1" >
-                    <tr>
-                        <td>
-                            <label for="username">User Name</label>
-                        </td>
-                        <td>
-                            <input type="text" name="username" id="username">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="userpassword">Password</label>
-                        </td>
-                        <td>
-                            <input type="password" name="userpassword" id="userpassword">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input type="submit" value="Submit"/>
-                        </td>
-                        <td>
-                            <input type="reset" value="Reset"/>
-                        </td>
-                    </tr>
-                </table>
-            </form>
+            <h1>Login Validation Form</h1>
+            <div class="centered">
+                <form action="user.php" method="POST" name="contactForm">
+                    <table border="1" >
+                        <tr>
+                            <td>
+                                <label for="username">User Name</label>
+                            </td>
+                            <td>
+                                <input type="text" name="username" id="username">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="userpassword">Password</label>
+                            </td>
+                            <td>
+                                <input type="password" name="userpassword" id="userpassword">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="submit" value="Submit"/>
+                            </td>
+                            <td>
+                                <input type="reset" value="Reset"/>
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
         </center></body>
     </html>
 _END;
--- user_validation/user.php
+++ user_validation/user.php
@@ -1,6 +1,19 @@
 <?php
 include_once "connect.php";
 
+
+echo <<<_END
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="style.css">
+            <title>Login Form - User Validation</title>
+        </head>
+    </html>
+_END;
+
 // Grab user info
 if (isset($_POST['username'], $_POST['userpassword'])) {
     $username = $_POST["username"];
